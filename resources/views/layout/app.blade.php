76781ae8bf0fb4b3ad347de5dc5d39802d47282d
Noppawit Thairungroj <noppawit.trr@gmail.com>
2017-06-13 01:29:51 +0700

    add nav bar

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -2,9 +2,9 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width,height=device-height,initial-scale=1">
     <title>@yield('title')</title>
     <link rel="stylesheet" href="/css/app.css">
+    <script src="/js/app.js"></script>
   </head>
   <body>
     @include('include.nav')
--- resources/views/timesheet.blade.php
+++ resources/views/timesheet.blade.php
@@ -3,4 +3,14 @@
 @section('title', 'Timesheet')
 
 @section('content')
+
+<div class="container">
+  <div class="row">
+    <div class="col-md-8 col-md-offset-2">
+      <h2>Timesheet</h2>
+      <hr>
+    </div>
+  </div>
+</div>
+
 @endsection

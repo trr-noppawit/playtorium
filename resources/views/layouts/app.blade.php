1e8aba31073e4af50d0bca1d97a8c711d3dc6e3e
MINTRA NANKONGNAEB <mind_lover_soul@hotmail.com>
2017-06-14 15:05:01 +0700

    meen

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,12 +2,12 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>@yield('title')</title>
+    <title>Acme</title>
     <link rel="stylesheet" href="/css/app.css">
-    <script src="/js/app.js"></script>
   </head>
   <body>
-    @include('include.nav')
     @yield('content')
+
+    @include('inc.sidebar')
   </body>
 </html>

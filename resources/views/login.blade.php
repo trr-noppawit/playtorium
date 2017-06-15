2b99c3810096012eaed189d1361cd75f6186b25b
5730447921-PW <bas_kung02@hotmail.com>
2017-06-15 09:17:19 +0700

    Create login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -21,6 +21,7 @@
         <div class="row">
           <div class="col-md-4 col-md-offset-4">
             <form action="">
+              <h1>Hello!</h1>
               <div class="form-group">
                 <input type="email" class="form-control" id="email" placeholder="E-mail" name="email">
               </div>
